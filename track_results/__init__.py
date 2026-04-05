4841a454349a70335e1948e64ea604693225f3bd
Joao Hespanha <hespanha@ece.ucsb.edu>
2026-04-05 10:55:47 -0700

    updated version number

--- track_results/__init__.py
+++ track_results/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["interesting_columns", "flatten_dict", "TrackResults"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .track_results import TrackResults, interesting_columns, flatten_dict
