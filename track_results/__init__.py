a16dc166303904d45c9403d39e6d891b50e45f2b
Joao Hespanha <hespanha@ece.ucsb.edu>
2026-06-27 00:15:50 -0700

    fixed __init__

--- track_results/__init__.py
+++ track_results/__init__.py
@@ -13,4 +13,12 @@ import importlib.metadata
 # Fetches the version assigned during installation dynamically
 __version__ = importlib.metadata.version("track_results")
 
-from .track_results import TrackResults, interesting_columns, flatten_dict
+from .track_results import (
+    TrackResults,
+    interesting_columns,
+    flatten_dict,
+    savefig_to_binary,
+    binary_to_pdf,
+    savefig_pickle2binary,
+    pickle2binary_to_fig,
+)
